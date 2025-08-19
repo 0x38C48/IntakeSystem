b2cd7f44c22e6840978f5e102e106d68c51d3624
Lunacaty <erocats@qq.com>
2025-08-19 22:14:10 +0800

    111

--- IntakeSystem/src/main/java/com/student_online/IntakeSystem/controller/TestController.java
+++ IntakeSystem/src/main/java/com/student_online/IntakeSystem/controller/TestController.java
@@ -1,12 +1,13 @@
 package com.student_online.IntakeSystem.controller;
 
+import com.student_online.IntakeSystem.model.vo.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public String test() {
-        return "Hello World";
+    public Result test() {
+        return Result.success(1,"success");
     }
 }
