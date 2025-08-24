416c22aa10e13c41dfb8f3ad0d47fed76a4e1c0f
Lunacaty <erocats@qq.com>
2025-08-24 12:52:20 +0800

    尝试修复跨域问题

--- IntakeSystem/src/main/java/com/student_online/IntakeSystem/config/OriginConfig.java
+++ IntakeSystem/src/main/java/com/student_online/IntakeSystem/config/OriginConfig.java
@@ -5,23 +5,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OriginConfig {
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration cors = new CorsConfiguration();
+//        //允许所有请求源
+//        cors.addAllowedOriginPattern("*");
+//        //允许所有请求头
+//        cors.addAllowedHeader("*");
+//        //允许所有HTTP方法
+//        cors.addAllowedMethod("*");
+//        //允许跨域发送cookie
+//        cors.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**",cors);  //封装
+//        return new CorsFilter(source);
+//    }
+    
     @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration cors = new CorsConfiguration();
-        //允许所有请求源
-        cors.addAllowedOriginPattern("*");
-        //允许所有请求头
-        cors.addAllowedHeader("*");
-        //允许所有HTTP方法
-        cors.addAllowedMethod("*");
-        //允许跨域发送cookie
-        cors.setAllowCredentials(true);
-        
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",cors);  //封装
-        return new CorsFilter(source);
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
