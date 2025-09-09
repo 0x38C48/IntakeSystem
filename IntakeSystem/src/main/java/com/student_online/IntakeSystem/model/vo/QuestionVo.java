dbd1573927729f650adf166e83c926fce33ea339
NonForgetter <wu060729@163.com>
2025-09-09 15:37:06 +0800

    bugfix

--- IntakeSystem/src/main/java/com/student_online/IntakeSystem/model/vo/QuestionVo.java
+++ IntakeSystem/src/main/java/com/student_online/IntakeSystem/model/vo/QuestionVo.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class QuestionVo extends Question {
 
 
-    private List<Option> options;
+    private List<Option> option;
 
 
     private Integer type;//题目类型
@@ -30,7 +30,7 @@ public class QuestionVo extends Question {
         setQuestionnaireId(question.getQuestionnaireId());
         setSort(question.getSort());
         this.type = question.getType();
-        this.options = option;
+        this.option = option;
     }
 
     @Override
@@ -41,7 +41,7 @@ public class QuestionVo extends Question {
                 ", 问题类型:" + super.getType() +
                 ", 问题内容:'" + super.getContent() + '\'' +
                 ", 问题序号:'" + super.getSort() + '\'' +
-                ", Option=" + options +
+                ", Option=" + option +
                 "}]\n";
     }
 }
