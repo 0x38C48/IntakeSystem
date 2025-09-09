4ecf7a136cf613f307a48b456f994c3d74de40d9
Lunacaty <erocats@qq.com>
2025-09-09 13:47:29 +0800

    2

--- IntakeSystem/src/main/java/com/student_online/IntakeSystem/model/dto/QuestionnaireInfoDto.java
+++ IntakeSystem/src/main/java/com/student_online/IntakeSystem/model/dto/QuestionnaireInfoDto.java
@@ -1,4 +1,14 @@
 package com.student_online.IntakeSystem.model.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
 public class QuestionnaireInfoDto {
+    private String username;
+    private String name;
+    private LocalDateTime updateTime;
+    private List<String> answerContent;
 }
